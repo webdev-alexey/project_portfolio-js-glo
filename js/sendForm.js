036bdca73f3f02ec751d0bcfa8920c381feb543f
alexey <alexey.anasov@yandex.by>
2025-02-16 15:17:45 +0300

    реализация отправка формы

---
+++ js/sendForm.js
@@ -0,0 +1,40 @@
+const forms = document.querySelectorAll("form");
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const body = {};
+
+    formData.append("form", form.classList.value);
+
+    formData.forEach((value, field) => {
+      body[field] = value;
+    });
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      })
+      .then((data) => {
+        alert("Данные отправлены успешно!");
+      })
+      .catch((error) => {
+        alert("Данные отправлены с ошибкой!" + error.message);
+      })
+      .finally(() => {
+        form.reset();
+      });
+  });
+});
